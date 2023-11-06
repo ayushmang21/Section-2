0a3192c9a55fcd08a5dc828b0b07ce29736db243
Ayushman Gupta <146174821+ayushmang21@users.noreply.github.com>
2023-11-07 00:24:37 +0530

    about us

--- Frontend/src/components/AboutUs.jsx
+++ Frontend/src/components/AboutUs.jsx
@@ -5,14 +5,24 @@ const AboutUs = () => {
   return (
     <div className='body'>
 
-        <div className='title'>
-            <h1>About Us</h1>
+        <div className='title container w-100 my-5'>
+            <p>About Us</p>
         </div>
         
-        <div className='sub-title'>
-            <h2>Qwerty</h2>
+        <div className='sub-title container w-75 my-5'>
+            <p>Our Story</p>
         </div>
-        
+
+        <div className='story container w-50 my-5'>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui pariatur rerum fugit eveniet quae, fuga neque eos consequuntur earum veritatis quisquam cum. Eaque nemo repellat uferhvij  urhiifvherfj h frhnwief hq3 hfr3q n frjw fhjle irfhj nn frnw fhjlw rg f   jo ftg  gj  le rr fj frjawe fj;ewirvj lair jlei fja ue hfgenjvinvnv j  ij lij glijer tg   fugiat fugit distinctio nesciunt. Repudiandae.</p>
+        </div>
+
+        <div className='my-5'>
+            <img className='founder' src="https://images.pexels.com/photos/18399715/pexels-photo-18399715/free-photo-of-blonde-in-cap-and-jacket.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Not Found" />
+            <h2 className='mt-4'>Alice Blake</h2>
+            <h5>Founder, CEO</h5>
+        </div>
+
     </div>
   )
 }
