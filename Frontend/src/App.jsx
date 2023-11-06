1409bb158424746afc0d6eb69ad2178a58c1b7ce
Ayushman Gupta <146174821+ayushmang21@users.noreply.github.com>
2023-11-06 20:50:24 +0530

    frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './App.css'
+import Home from './components/home';
+import Login from './components/login';
+import SignUp from './components/SignUp';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+
+      <div>
+        
+        <BrowserRouter>
+            <Routes>
+
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/signup' element={<SignUp />} />
+              <Route path='/aboutus' element={<AboutUs />} />
+              <Route path='/contactus' element={<ContactUs />} />
+
+            </Routes>
+        </BrowserRouter>
+      </div>
+
+    </>
+  )
+}
+
+export default App;
---
+++ Frontend/src/components/AboutUs.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './aboutus.css'
+
+const AboutUs = () => {
+  return (
+    <div className='body'>
+
+        <div className='title'>
+            <h1>About Us</h1>
+        </div>
+        
+        <div className='sub-title'>
+            <h2>Qwerty</h2>
+        </div>
+        
+    </div>
+  )
+}
+
+export default AboutUs;
