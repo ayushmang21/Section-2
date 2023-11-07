3f85fc7e2dd38618e5c49166ca998b57813be5e5
Ayushman Gupta <146174821+ayushmang21@users.noreply.github.com>
2023-11-07 11:31:32 +0530

    contact_us

--- Frontend/src/components/ContactUs.jsx
+++ Frontend/src/components/ContactUs.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import './contactus.css'
 
 const ContactUs = () => {
   return (
-    <div>
-        <h1>Contact Us</h1>
+    <div className='body'>
+        <div className='head'>
+          <p>Contact Us</p>
+        </div>
+        
+        <form action="#" className='container'>
+          <label htmlFor="name">Your Name</label>
+          <input type="text" placeholder='Enter Name' className='myinput' required/>
+
+          <label htmlFor="mail">Your Email</label>
+          <input type="email" placeholder='Enter Email' className='myinput' required/>
+
+          <label htmlFor="help">How can we help you ?</label>
+          <textarea className="help" placeholder='Write here...' cols="30" rows="10" required></textarea>
+
+          <div>
+            <button type="submit" className='btn'>Submit</button>
+          </div>
+
+        </form>
     </div>
   )
 }
