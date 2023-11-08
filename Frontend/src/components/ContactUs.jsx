45c6fde2bf2cb0a70ee5e9030df3c8c0ef2f2efa
Ayushman Gupta <146174821+ayushmang21@users.noreply.github.com>
2023-11-08 12:46:50 +0530

    w

--- Frontend/src/components/ContactUs.jsx
+++ Frontend/src/components/ContactUs.jsx
@@ -5,30 +5,29 @@ const ContactUs = () => {
   return (
     <div className='b'>
 
-        <div className='head'>
-          <p>Contact Us</p>
-        </div>
+      <div className="myform my-5">
 
-        <div className='container cont'>
+        <h3 className="form-title">Contact Us</h3>
 
-          <form action="">
+          <form action="contact form.html">
 
-            <label htmlFor="name">Your Name</label>
-            <input type="text" placeholder='Enter Name' className='myinput' required/>
+            <label>Your Name</label>
+            <input type="text" className="myinput" required="" />
 
-            <label htmlFor="mail">Your Email</label>
-            <input type="email" placeholder='Enter Email' className='myinput' required/>
+            <label>Your Email</label>
+            <input type="text" className="myinput" required="" />
 
-            <label htmlFor="help">How can we help you ?</label>
-            <textarea className="help" placeholder='Write here...' cols="30" rows="10" required></textarea>
+            <label>How can we help you ?</label>
+            <textarea className="help" cols={30} rows={7} required="" defaultValue={""}/>
 
             <div>
-              <button type="submit" className='btn'>Submit</button>
+              <button className="mybtn btnhov">Submit</button>
             </div>
-
+            
           </form>
 
-        </div>
+      </div>
+
 
     </div>
   )
