05c0824a367b2cb4f3c4f86631b890dae4a68fc0
Ayushman Gupta <146174821+ayushmang21@users.noreply.github.com>
2023-11-13 12:43:44 +0530

    state

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './App.css'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Home from './components/home';
 import Login from './components/login';
 import SignUp from './components/SignUp';
 import AboutUs from './components/AboutUs';
 import ContactUs from './components/ContactUs';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Navbar from './components/navbar';
 import EventHandling from './components/EventHandling';
 import NotFound from './components/NotFound';
+import StateManagement from './components/StateManagement';
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
               <Route path='/aboutus' element={<AboutUs />} />
               <Route path='/contactus' element={<ContactUs />} />
               <Route path='/event' element={<EventHandling />} />
+              <Route path='/state' element={<StateManagement />} />
               <Route path='*' element={<NotFound />} />
 
             </Routes>
--- Frontend/src/components/EventHandling.jsx
+++ Frontend/src/components/EventHandling.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const EventHandling = () => {
   return (
-    <div>EventHandling</div>
+    <div className='container'>
+      <h1 className='text-center'>Event Handling</h1>
+      <hr />
+
+      <button className='btn btn-primary' onClick={ () => {prompt('Nice alert!!')} }>My Button</button>
+
+      <input type="text" className='form-control mt-4' onChange={ (event) => { console.log(event.target.value); } }/>
+
+      <input type="color" onChange={ (e) => { document.body.style.backgroundColor = e.target.value; } } />
+
+      
+
+
+    </div>
   )
 }
 
