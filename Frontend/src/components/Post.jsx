bff9b393201746fb126ab8c7767c950958273d6b
Ayushman Gupta <146174821+ayushmang21@users.noreply.github.com>
2023-11-19 00:08:56 +0530

    post

--- Frontend/src/components/Post.jsx
+++ Frontend/src/components/Post.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Post = () => {
+
+    const [Likes, setLikes] = useState(0)
+
   return (
-    <div>
-        <div className=' container'>
-            <div className='card'>
-                <div></div>
+    <div style={{fontFamily: 'Montserrat'}}>
+
+        <div className='container d-flex justify-content-center'>
+
+            <div className='card my-5 w-50 border-dark'>
+
+            {/* head */}
+                <div className='card-header d-flex justify-content-between align-items-center border-dark-subtle'>
+                    <div className='d-inline-flex align-items-center'>
+                        <img src="https://fiorecommunications.com/wp-content/uploads/2019/05/instagram-blog-person-icon-600x600.jpg"
+                        height={40} alt="" 
+                        className='rounded-circle' />
+                        <p className='ms-2 fw-medium fs-5'>Lorem ipsum <br /> <p className='fw-normal fs-6'>Lorem ipsum, dolor</p> </p>
+                    </div>
+                    <div>
+                        <p className='fw-bold fs-3'>⁝</p>
+                    </div>
+                </div>
+
+            {/* body */}
+                <div className='card-body d-flex justify-content-center align-items-center'>
+                    <div>
+                        <img src="https://images.pexels.com/photos/4104772/pexels-photo-4104772.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+                         alt="" className='' style={{}} />
+                    </div>
+                </div>
+
+            {/* foot */}
+                <div className='card-footer border-dark-subtle'>
+                    <div className='my-2'>
+                        <button className='btn btn-danger fs-5' onClick={ () => {setLikes(Likes+1);}} >❤</button>
+                        <p className='mt-2'>Liked by Lorem and {Likes} others</p>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
--- Frontend/src/components/StateManagement.jsx
+++ Frontend/src/components/StateManagement.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const StateManagement = () => {
 
-    let count = 0;
+    // let count = 0;
 
     // a - Read-Only ; b - Update Func
     // const [a,b] =useState(0);
@@ -10,27 +10,27 @@ const StateManagement = () => {
     // const [Likes, setLikes] = useState(0);
     const [Likes, setLikes] = useState(0);
 
-    const [imglink, setImglink] = useState("");
+    // const [imglink, setImglink] = useState("");
 
   return (
     <div className='container'>
             <h1 className='text-center'>State Management</h1>
             <hr />
 
-            <h1>{count}</h1>
+            {/* <h1>{count}</h1> */}
 
-            <button className='btn btn-danger' onClick={ () => {count++; console.log(count);}}>Add 1</button>
+            {/* <button className='btn btn-danger' onClick={ () => {count++; console.log(count);}}>Add 1</button> */}
 
             <h1>Like : {Likes}</h1>
 
             <button className='btn btn-primary' onClick={ () => {setLikes(Likes+1);}} > Add Like</button>
 
-            <input type="text" className='form-control mt-5' onChange={(e) => {setImglink(e.target.value)}}/>
+            {/* <input type="text" className='form-control mt-5' onChange={(e) => {setImglink(e.target.value)}}/>
             <label>Insert Image Link Above</label>
 
             {/* <h3>{imglink}</h3> */}
 
-            <img className='d-block' src={imglink} alt="" />
+            {/* <img className='d-block' src={imglink} alt="" /> */}
     </div>
   )
 }
