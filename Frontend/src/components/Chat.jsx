bef2d2c9da594ad5d93e594ead68fb425ff2efcd
Ayushman Gupta <ayushman0021@gmail.com>
2023-11-21 11:42:32 +0530

    chat

--- Frontend/src/components/Chat.jsx
+++ Frontend/src/components/Chat.jsx
@@ -9,16 +9,27 @@ const Chat = () => {
 {/* Header */}
                 <div class="card-header bg-success d-flex justify-content-between align-items-center">
                     <img className='rounded-circle' src="https://th.bing.com/th/id/OIP.Nl5tCCLifM2hpDKI0h6L3QAAAA?rs=1&pid=ImgDetMain" alt="" height={40}/>
-
+                    <p></p>
                 </div>
 
                 <div class="card-body bg-success-subtle">
 
                 </div>
 {/* Footer */}
-                <div class="card-footer bg-success d-flex justify-content- align-items-center">
-                    <i class="bi bi-emoji-smile"></i>
-                    <input type="text" placeholder='Message' className='rounded-pill p-2 ps-3 border-0 w-100'/>
+                <div class="card-footer bg-success d-flex align-items-center justify-content-between">
+
+                    <div className='d-inline-flex align-items-center bg-white rounded-pill px-3 w-100'>
+                        <p className='fs-4'><i class="bi bi-emoji-smile-fill"></i></p>
+                        <input type="text" placeholder='Message' className='p-2 ms-2 border-0 text-muted w-75'/>
+                        <p className='fs-4 ps-2'><i class="bi bi-paperclip"></i></p>
+                        <p className='fs-4 ps-2'><i class="bi bi-currency-rupee"></i></p>
+                        <p className='fs-4 ps-2 pe-2'><i class="bi bi-camera-fill"></i></p>
+                    </div>
+
+                    <div className='ms-4 me-3 rounded-pill bg-white'>
+                        <p className='fs-4 text-success'><i class="bi bi-mic-fill"></i></p>
+                    </div>
+
                 </div>
 
             </div>
