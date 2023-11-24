03e854d8aaae78bef9583c4a184cc543ff39a41e
Ayushman Gupta <ayushman0021@gmail.com>
2023-11-24 11:19:19 +0530

    create

--- Frontend/src/components/CreatePost.jsx
+++ Frontend/src/components/CreatePost.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
 
 const CreatePost = () => {
-  return (
-    <div>
-        <div className='container mt-5 '>
-            <div className='card border-3 border-dark'>
-                <div className='card-body my-4'>
-                    
-                    <div className=''> 
-                        <label htmlFor="content" className='mb-3 fs-3'>Content</label>
-                        <textarea className='form-control border-2 border-dark' name="" id="" cols="30" rows="10"></textarea>
-                    </div>
+    return (
+        <div style={{ fontFamily: 'Montserrat' }}>
+            <div className='col-md-6 mx-auto'>
+                <div className='container mt-4 '>
+                    <div className='card border-3 border-dark '>
+                        <div className='card-body m-4'>
 
-                    <div className=''>
-                        <label htmlFor="photo" className='my-3 fs-3'>Image</label>
-                        <input className='form-control border-2 border-dark' type="text" />
-                    </div>
 
+                            <label htmlFor="content" className='mb-3 fs-3'>Content</label>
+                            <textarea className='form-control border-2 border-dark' name="" id="" cols="30" rows="7"></textarea>
+
+                            <label htmlFor="photo" className='my-3 fs-3'>Image</label>
+                            <input className='form-control border-2 border-dark' type="text" />
+
+                            <button className='btn w-75 mt-5' style={{ backgroundColor: '#5627CC', color: 'white' }}>Create</button>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default CreatePost;
