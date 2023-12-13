ee1c1d6f2a0641a2a0a17a200c92cd706409eee3
Ayushman Gupta <ayushman0021@gmail.com>
2023-12-13 22:49:36 +0530

    contact

--- Frontend/src/components/ContactUs.jsx
+++ Frontend/src/components/ContactUs.jsx
@@ -5,20 +5,20 @@ const ContactUs = () => {
   return (
     <div className='b'>
 
-      <div className="myform my-5">
+      <div className="myform my-5 rounded-2">
 
         <h3 className="form-title">Contact Us</h3>
 
           <form action="contact form.html">
 
             <label>Your Name</label>
-            <input type="text" className="myinput" required="" />
+            <input type="text" className="myinput rounded-2" />
 
             <label>Your Email</label>
-            <input type="text" className="myinput" required="" />
+            <input type="text" className="myinput rounded-2" />
 
             <label>How can we help you ?</label>
-            <textarea className="help" cols={30} rows={7} required="" defaultValue={""}/>
+            <textarea className="help rounded-2" cols={30} rows={7}/>
 
             <div>
               <button className="mybtn btnhov">Submit</button>
