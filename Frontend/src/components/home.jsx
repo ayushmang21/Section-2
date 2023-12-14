75f5a38b8a8802bd96e98ce0d21e5edb4bae55bd
Ayushman Gupta <ayushman0021@gmail.com>
2023-12-14 19:57:06 +0530

    home

--- Frontend/src/components/home.jsx
+++ Frontend/src/components/home.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div style={{ fontFamily: 'Montserrat' }}>
+    <div className='homepg' style={{ fontFamily: 'Montserrat' }}>
 
-      <div style={{backgroundColor:'#D9C0C1'}} className='py-1'>
+      <div style={{backgroundColr:'#D9C0C1'}} className='py-1'>
         <h1 className='my-4 text-center' style={{ fontSize: 70, }}>Welcome to&#160;
-          <span style={{ color: '#391b7f', fontFamily: 'monospace', textDecoration: 'underline' }}>
+          <span style={{ color: 'white', fontFamily: 'monospace', textDecoration: 'underline' }}>
             Voilet.io
           </span>
         </h1>
@@ -18,24 +18,24 @@ const Home = () => {
         </p>
       </div>
 
-      <div className='mt-5 py-2' style={{backgroundColor:'#D9C0C1'}}>
+      <div className='my-5 py-2' style={{backgrounColor:'#D9C0C1'}}>
         <p className='fs-3 fw-medium text-center'>
           Are you a First-Timer...?&#160;
           <span>
             <br />
-            <a href="SignUp" style={{ color: '#9F5ECF' }}>Sign Up Here</a>
+            <a href="SignUp" style={{ color: 'white' }}>Sign Up Here</a>
             <br />
           </span>
           Or
           <span>
             <br />
-            <a href="Login" style={{ color: '#9F5ECF' }}>Login Here</a>
+            <a href="Login" style={{ color: 'white' }}>Login Here</a>
           </span>
         </p>
       </div>
 
       <div>
- 
+        
       </div>
 
     </div>
