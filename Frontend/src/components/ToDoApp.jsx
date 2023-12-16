00486f4eb967c4651a80525fdb4ca798f4244fb8
Ayushman Gupta <ayushman0021@gmail.com>
2023-12-16 23:28:23 +0530

    todo

--- Frontend/src/components/ToDoApp.jsx
+++ Frontend/src/components/ToDoApp.jsx
@@ -8,12 +8,27 @@ const ToDoApp = () => {
         if (e.code == 'Enter') {
             console.log(e.target.value);
 
-            setTodolist([e.target.value, ...todolist]);
-            console.log([e.target.value, ...todolist]);
+            setTodolist([{ text: e.target.value, completed: false }, ...todolist]);
+            console.log([{ text: e.target.value, completed: false }, ...todolist]);
 
             e.target.value = "";
         }
     }
+
+    const completeTask = (index) => {
+        console.log(index);
+        const temp = todolist;
+        temp[index].completed = true;
+        setTodolist([...temp]);
+        console.log(temp);
+    }
+
+    const deleteTask = (index) => {
+        const temp = todolist;
+        temp.splice(index, 1);
+        setTodolist([...temp]);
+    }
+
     return (
         <div style={{ fontFamily: 'Montserrat' }}>
             <div className='container'>
@@ -26,14 +41,21 @@ const ToDoApp = () => {
                     <div className='card-header p-4' style={{ backgroundColor: '#391b7f' }}>
                         <input type="text" onKeyDown={addTask} className='form-control border-0 focus-ring focus-ring-light' style={{ backgroundColor: 'D9C0C1' }} placeholder='Enter Task Here and Press Enter ↵' />
                     </div>
-                    
+
                     <div className='card-body' style={{ backgroundColor: '#D9C0C1' }}>
                         {
-                            todolist.map((task) => {
+                            todolist.map((task, index) => {
                                 return (
                                     <div className='d-flex justify-content-between align-items-center mt-2 fw-medium fs-4'>
-                                        <p> {task}  </p>
-                                        <button className='btn btn-sm fw-medium text-white' style={{backgroundColor:'#C71837'}}>Delete</button>
+                                        <p className={task.completed ? 'text-success task-done' : 'fw-bold'}>
+                                            {task.text}
+                                            {task.completed ? <span className='badge text-bg-success ms-3'>Completed</span> : ''}
+                                        </p>
+
+                                        <div>
+                                            <button className='btn btn-primary me-3' onClick={() => { completeTask(index) }} >Complete</button>
+                                            <button className='btn text-white' onClick={() => { deleteTask(index) }} style={{ backgroundColor: '#C71837' }}>Delete</button>
+                                        </div>
                                     </div>
                                 )
                             })}
