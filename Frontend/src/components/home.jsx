5a5aa46f2a5bb3c4326592585338912202cf0888
Ayushman Gupta <ayushman0021@gmail.com>
2023-12-17 23:34:40 +0530

    grad

--- Frontend/src/components/home.jsx
+++ Frontend/src/components/home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className='homepg' style={{ fontFamily: 'Montserrat' }}>
 
-      <div style={{backgroundColr:'#D9C0C1'}} className='py-1'>
+      <div style={{ backgroundColr: '#D9C0C1' }} className='py-1'>
         <h1 className='my-4 text-center' style={{ fontSize: 70, }}>Welcome to&#160;
           <span style={{ color: 'white', fontFamily: 'monospace', textDecoration: 'underline' }}>
             Voilet.io
@@ -18,24 +18,22 @@ const Home = () => {
         </p>
       </div>
 
-      <div className='my-5 py-2' style={{backgrounColor:'#D9C0C1'}}>
+      <div className='mt-5 mb-4 py-2' style={{ backgrounColor: '#D9C0C1' }}>
         <p className='fs-3 fw-medium text-center'>
           Are you a First-Timer...?&#160;
-          <span>
-            <br />
-            <a href="SignUp" style={{ color: 'white' }}>Sign Up Here</a>
-            <br />
-          </span>
-          Or
-          <span>
-            <br />
-            <a href="Login" style={{ color: 'white' }}>Login Here</a>
-          </span>
         </p>
       </div>
 
-      <div>
-        
+      <div className='py-2 fw-medium text-center'>
+
+        <a className='fs-2' href="SignUp" style={{ color: 'white' }}>Sign Up Here</a>
+
+        <div className='my-3 fs-3'>
+          Or
+        </div>
+
+        <a className='fs-2' href="Login" style={{ color: 'white' }}>Login Here</a>
+
       </div>
 
     </div>
